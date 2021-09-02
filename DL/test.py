c7aa22537e74e78d6236baddfb34fbb224693721
essential2189 <essential2189@gmail.com>
2021-09-02 14:32:10 +0900

    ML_Study

---
+++ DL/test.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+
+epochs = 5
+
+# Define network architecture
+class MyModel(tf.keras.Model):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.flatten = tf.keras.layers.Flatten(input_shape=(28, 28))
+        self.d1 = tf.keras.layers.Dense(128, activation='sigmoid')
+        self.d2 = tf.keras.layers.Dense(10, activation='softmax')
+
+    def call(self, x):
+        x = self.flatten(x)
+        x = self.d1(x)
+        return self.d2(x)
+
+
+# Implement training loop
+@tf.function
+def train_step(model, images, labels, loss_object, optimizer, train_loss, train_accuracy):
+    with tf.GradientTape() as tape:
+        predictions = model(images)
+        loss = loss_object(labels, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    train_loss(loss)
+    train_accuracy(labels, predictions)
+
+
+# Implement algorithm test (test loop)
+@tf.function
+def test_step(model, images, labels, loss_object, test_loss, test_accuracy):
+    prediction = model(images)
+
+    t_loss = loss_object(labels, prediction)
+    test_loss(t_loss)
+    test_accuracy(labels, prediction)
+
+
+# Import and organize dataset (데이터셋 가져오고 정리)
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+x_train = x_train[..., tf.newaxis]
+x_test = x_test[..., tf.newaxis]
+
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(32)
+test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+
+
+# Create model (네트워크 생성)
+model = MyModel()
+
+# Define loss and optimizer (손실함수, 최적화 알고리즘 정의)
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+optimizer = tf.keras.optimizers.Adam()
+
+# Define performance metrics (알고리즘 평가지표 설정)
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseTopKCategoricalAccuracy(name='train_accuracy')
+
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.SparseTopKCategoricalAccuracy(name='test_accuracy')
+
+
+# Do training loop and test
+for epoch in range(epochs):
+    for images, labels in train_ds:
+        train_step(model, images, labels, loss_object, optimizer, train_loss, train_accuracy)
+
+    for test_images, test_labels in test_ds:
+        test_step(model, test_images, test_labels, loss_object, test_loss, test_accuracy)
+
+    template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+    print(template.format(epoch+1,
+                          train_loss.result(),
+                          train_accuracy.result() * 100,
+                          test_loss.result(),
+                          test_accuracy.result() * 100))
+
+
